dc534a2050df01bade115986e32bcb9eadc84cff
Mikalai Zarazaka <golmtz@gmail.com>
2022-06-14 21:58:28 +0300

    add react router

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './scss/app.scss';
+import {Header} from "./Components/Header";
+import {Home} from "./Components/pages/Home";
+import {NotFound} from "./Components/pages/NotFound";
+import {Route, Routes} from "react-router-dom";
+import {Cart} from "./Components/pages/Cart";
+
+function App() {
+
+
+    return (
+        <div className="wrapper">
+            <Header/>
+            <div className="content">
+                <div className="container">
+                    <Routes>
+                        <Route path={'/'} element={<Home/>}/>
+                        <Route path={'*'} element={<NotFound/>}/>
+                        <Route path={'сфке'} element={<Cart/>}/>
+                    </Routes>
+                </div>
+            </div>
+        </div>
+    )
+        ;
+}
+
+export default App;
---
+++ src/Components/pages/Home.jsx
@@ -0,0 +1,37 @@
+import React, {useEffect, useState} from 'react';
+import {Categories} from "../Categories";
+import {Sort} from "../Sort";
+import PizzaSkeleton from "../../common/component/PizzaSkeleton";
+import {PizzaBlock} from "../PizzaBlock";
+
+export const Home = () => {
+    const [items, setItems] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+
+    useEffect(() => {
+        fetch('https://62a78e03bedc4ca6d7cad1f0.mockapi.io/items')
+            .then((res) => res.json())
+            .then(arr => {
+                setTimeout(() => {
+                    setItems(arr)
+                    setIsLoading(false)
+                }, 1000)
+            })
+    }, [])
+    return (
+        <>
+            <div className="content__top">
+                <Categories/>
+                <Sort/>
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                {isLoading
+                    ? [...new Array(6)].map((_, index) => <PizzaSkeleton key={index}/>)
+                    : items.map(obj => <PizzaBlock
+                        key={obj.id} {...obj}/>)}
+            </div>
+        </>
+    );
+};
+
